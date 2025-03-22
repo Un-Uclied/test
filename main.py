d0accd10b844f9127b990fab0459c843885c2ad1
Un-Uclied <pengdraw722@gmail.com>
2025-03-22 13:27:34 +0900

    oi oi oi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import raylibpy as rl
 
 class Game:
     def __init__(self):
-        rl.init_window(1900, 800, "test")
+        rl.init_window(1600, 800, "test")
 
         self.scenes = {
             "main_game" : MainGameScene(self)
@@ -29,6 +29,12 @@ class MainGameScene:
 
         self.camera = rl.Camera2D(rl.Vector2(rl.get_screen_width() / 2, rl.get_screen_height() / 2), rl.Vector2(), 0, 1)
 
+        self.tile_size = 50
+        self.backpack_rects = [[] for i in range(10)]
+        for y in range(10):
+            for x in range(10):
+                self.backpack_rects[y].append(rl.Rectangle(x * 50 + 100, y * 50 + 150, 50, 50))
+
     def update(self):
         pass
 
@@ -40,7 +46,17 @@ class MainGameScene:
 
         rl.end_mode2d()
         #ui render
-        rl.draw_rectangle_rec(rl.Rectangle(0, 0, 100, 100), rl.RED)
+        rl.draw_rectangle_rec(rl.Rectangle(50, 0, 600, rl.get_screen_height()), rl.BROWN)
+
+        rl.draw_rectangle_rec(rl.Rectangle(rl.get_screen_width() - 600 - 50, 0, 600, rl.get_screen_height()), rl.WHITE)
+        for y in range(10):
+            for x in range(10):
+                rl.draw_rectangle_rec(self.backpack_rects[y][x], rl.GREEN)
+        for y in range(9):
+            for x in range(9):
+                rl.draw_line(x * 50 + 100, y * 50 + 200, x * 50 + 100 + 100, y * 50 + 200, rl.RED)
+                rl.draw_line(x * 50 + 150, y * 50 + 150, x * 50  + 150, y * 50 + 100 + 150, rl.RED)
+            
         rl.end_drawing()
 if __name__ == "__main__":
     Game().run()
